5bb05592d7644bc7cc8db15b9cf1955e43aa7325
Moiz Qureshi <m.moiz782008@gmail.com>
2024-11-21 13:29:24 +0500

    commint
    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,10 @@ import Showcase from "./component/showcase";
 export default function Hero (){
   return(
     <main className="bg-stone-800 text-white max-w-screen-2xl mx-auto">
-      <div className="flex flex-col items-center text-center sm:text-start md:flex-row py-20 px-6 md:justify-around">
+      <div className="flex flex-col items-center text-center sm:text-start md:flex-row pt-10 pb-12 px-6 md:justify-around animat">
         <Image src={men} alt="menImage" width={500} height={400} className="md:hidden"/>
         <div className="px-6">
-          <h2 className="text-6xl font-bold pt-3 pb-12">Crafting <br />Architectural <br />Wonders</h2>
+          <h2 className="text-6xl font-bold pb-12">Crafting <br />Architectural <br />Wonders</h2>
           <p className="text-gray-300 pb-8 font-semibold">Your Ideas & Dreams Are Treansformed by us Into Long-<br />Lesting, Engineered Buildings.</p>
           <div>
             <a href="/contact"><button className="border-2 border-yellow-600 p-2 pr-12 pl-12 ml-7 mb-5 text-black font-semibold bg-yellow-500 hover:bg-stone-800 hover:text-white">Contect Us</button></a>
